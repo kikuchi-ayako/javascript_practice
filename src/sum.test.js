82099f8ccc4d44542da5f56329346d3bf3b87b9e
kikuchi-ayako <122848749+kikuchi-ayako@users.noreply.github.com>
2023-01-31 12:54:51 +0900

    コメントの練習

--- src/sum.test.js
+++ src/sum.test.js
@@ -3,3 +3,10 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('プログラムコメントの練習', () => {
+//エラーになる為、実行させないようにコメントにする。
+/*
+  expect(sum(1, 2)).toBe(4);
+*/
+});
